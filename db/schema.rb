18110197934071567d952eb6f2845e2670de00a7
Mauro Rocha <mauror@mauror.com.br>
2021-03-09 18:52:59 -0300

    WIP - Parte 3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_213720) do
+ActiveRecord::Schema.define(version: 2021_03_09_213423) do
 
   create_table "contents", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,24 @@ ActiveRecord::Schema.define(version: 2021_03_08_213720) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_contents_on_user_id"
+  end
+
+  create_table "tag_contents", force: :cascade do |t|
+    t.integer "tag_id", null: false
+    t.integer "content_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["content_id"], name: "index_tag_contents_on_content_id"
+    t.index ["tag_id"], name: "index_tag_contents_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name", "user_id"], name: "index_tags_on_name_and_user_id", unique: true
+    t.index ["user_id"], name: "index_tags_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +52,7 @@ ActiveRecord::Schema.define(version: 2021_03_08_213720) do
   end
 
   add_foreign_key "contents", "users"
+  add_foreign_key "tag_contents", "contents"
+  add_foreign_key "tag_contents", "tags"
+  add_foreign_key "tags", "users"
 end

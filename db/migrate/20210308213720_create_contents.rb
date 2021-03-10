299e77a1d9f11fd2f7252d8218f1a2888d229855
Mauro Rocha <mauror@mauror.com.br>
2021-03-10 19:14:17 -0300

    teste

--- db/migrate/20210308213720_create_contents.rb
+++ db/migrate/20210308213720_create_contents.rb
@@ -1,8 +1,8 @@
 class CreateContents < ActiveRecord::Migration[6.1]
   def change
     create_table :contents do |t|
-      t.string :title
-      t.string :description
+      t.string :title, null: false
+      t.string :description, null: false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps

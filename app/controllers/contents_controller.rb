674b65e6a63d80f94820ee2c3ec70fde8fdaf4c9
Mauro Rocha <mauror@mauror.com.br>
2021-03-11 18:20:26 -0300

    Crud feito

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,8 +1,11 @@
 class ContentsController < ApplicationController
     before_action :authenticate_user!
+    before_action :set_content, only: [:show, :edit, :update, :destroy]
     def index
         @contents = current_user.contents
     end
+
+    def show; end
 
     def new
         @content = Content.new
@@ -11,19 +14,49 @@ class ContentsController < ApplicationController
     def create
         @content = current_user.contents.build(content_params)
         if @content.save
+            associate_tags!
             redirect_to contents_path, notice: 'Content sucessfully created!'
         else
             render :new
         end
     end
 
-    def edit
-        @content = Content.find(params[:id])
+    def edit; end
+
+    def update
+
+        if @content.update(content_params)
+            associate_tags!
+            redirect_to contents_path, notice: 'Content sucessfully updated!'
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @content.destroy
+        redirect_to contents_path, notice: 'Content sucessfully deleted!'
     end
 
     private
+
+    def set_content
+        @content = Content.find(params[:id])
+    end
+
+    def tags_params
+        params.require(:content).permit(tags:[])[:tags].reject(&:blank?)
+    end
+
     def content_params
         params.require(:content).permit(:title, :description)
+    end
+
+    def associate_tags!
+        tags = tags_params.map do |tag_name|
+            current_user.tags.where(name: tag_name).first_or_initialize
+        end
+        @content.tags = tags
     end
   end
   
